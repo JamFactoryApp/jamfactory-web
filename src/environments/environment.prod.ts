4be25f311664b15df54c299d92ac285d59f2f4ee
Jonas Levin Weber <DrJoLe@users.noreply.github.com>
2020-10-10 14:56:11 +0200

    :bug: Use wss protocol in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   JAMFACTORY_API_URL: 'https://example.com',
-  JAMFACTORY_WS_URL: 'ws://example.com/ws'
+  JAMFACTORY_WS_URL: 'wss://example.com/ws'
 };
