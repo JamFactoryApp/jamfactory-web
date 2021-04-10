b1c70e259d4ad4a75b727636dc429bb0d1a528bf
Timo <timoenglishman@googlemail.com>
2021-04-10 12:48:16 +0200

    :art: Add ColorService

--- src/app/components/queue-song/queue-song.component.ts
+++ src/app/components/queue-song/queue-song.component.ts
@@ -2,12 +2,11 @@ import {Component, Input, OnInit, Output, ViewChild, ElementRef, AfterViewInit} 
 import {faHeart as iconVote} from '@fortawesome/free-solid-svg-icons';
 import {faHeart as iconNVote} from '@fortawesome/free-regular-svg-icons';
 import {QueueService} from '../../services/queue.service';
+import {ColorService} from '../../services/color.service';
 import SongWithoutId = JamFactoryApi.SongWithoutId;
 import FullTrack = Zmb3SpotifyApi.FullTrack;
 import PutQueueVoteRequest = JamFactoryApi.PutQueueVoteRequest;
 import TrackObjectFull = SpotifyApi.TrackObjectFull;
-
-type Vec3 = [number, number, number];
 
 declare var ColorThief: any;
 
@@ -47,10 +46,10 @@ export class QueueSongComponent implements OnInit {
   VibrantColor;
   MutedColor;
 
-  constructor(private queueService: QueueService) {}
+  constructor(private queueService: QueueService, private colorDiffService: ColorService) {}
 
   ngOnInit(): void {
-    console.log(this.queue);
+    console.log(this.songVoted);
   }
 
   getArtist(item): string {
@@ -72,112 +71,8 @@ export class QueueSongComponent implements OnInit {
 
     this.MutedColor = 'rgba(' + col[0] + ',' + col[1] + ',' + col[2] + ')';
 
-    const col2 = this.highestDiff(palette, col);
+    const col2 = this.colorDiffService.highestDiff(palette, col);
     this.VibrantColor = 'rgba(' + col2[0] + ',' + col2[1] + ',' + col2[2] + ')';
-  }
-
-  highestDiff(palette, col): any {
-    let tap = 0;
-    let mit = 0;
-
-    const rgbMain: Vec3 = col;
-
-    let farb;
-
-    palette.forEach(color => {
-      const rgbPal: Vec3 = color;
-      tap = this.rgbDiff(rgbPal, rgbMain);
-
-      if (tap >= mit) {
-        mit = tap;
-        farb = rgbPal;
-      }
-
-    });
-
-    return farb;
-  }
-
-  rgbDiff(rgb1: Vec3, rgb2: Vec3): number {
-    const lab1 = this.rgbToCIELab(rgb1[0], rgb1[1], rgb1[2]);
-    const lab2 = this.rgbToCIELab(rgb2[0], rgb2[1], rgb2[2]);
-    return this.deltaE94(lab1, lab2);
-  }
-
-  rgbToCIELab(r: number, g: number, b: number): Vec3 {
-    const [x, y, z] = this.rgbToXyz(r, g, b);
-    return this.xyzToCIELab(x, y, z);
-  }
-
-  rgbToXyz(r: number, g: number, b: number): Vec3 {
-    r /= 255;
-    g /= 255;
-    b /= 255;
-    r = r > 0.04045 ? Math.pow((r + 0.005) / 1.055, 2.4) : r / 12.92;
-    g = g > 0.04045 ? Math.pow((g + 0.005) / 1.055, 2.4) : g / 12.92;
-    b = b > 0.04045 ? Math.pow((b + 0.005) / 1.055, 2.4) : b / 12.92;
-
-    r *= 100;
-    g *= 100;
-    b *= 100;
-
-    const x = r * 0.4124 + g * 0.3576 + b * 0.1805;
-    const y = r * 0.2126 + g * 0.7152 + b * 0.0722;
-    const z = r * 0.0193 + g * 0.1192 + b * 0.9505;
-
-    return [x, y, z];
-  }
-
-  xyzToCIELab(x: number, y: number, z: number): Vec3 {
-    const REF_X = 95.047;
-    const REF_Y = 100;
-    const REF_Z = 108.883;
-
-    x /= REF_X;
-    y /= REF_Y;
-    z /= REF_Z;
-
-    x = x > 0.008856 ? Math.pow(x, 1 / 3) : 7.787 * x + 16 / 116;
-    y = y > 0.008856 ? Math.pow(y, 1 / 3) : 7.787 * y + 16 / 116;
-    z = z > 0.008856 ? Math.pow(z, 1 / 3) : 7.787 * z + 16 / 116;
-
-    const L = 116 * y - 16;
-    const a = 500 * (x - y);
-    const b = 200 * (y - z);
-
-    return [L, a, b];
-  }
-
-  deltaE94(lab1: Vec3, lab2: Vec3): number {
-    const WEIGHT_L = 1;
-    const WEIGHT_C = 1;
-    const WEIGHT_H = 1;
-
-    const [L1, a1, b1] = lab1;
-    const [L2, a2, b2] = lab2;
-    const dL = L1 - L2;
-    const da = a1 - a2;
-    const db = b1 - b2;
-
-    const xC1 = Math.sqrt(a1 * a1 + b1 * b1);
-    const xC2 = Math.sqrt(a2 * a2 + b2 * b2);
-
-    let xDL = L2 - L1;
-    let xDC = xC2 - xC1;
-    const xDE = Math.sqrt(dL * dL + da * da + db * db);
-
-    let xDH = (Math.sqrt(xDE) > Math.sqrt(Math.abs(xDL)) + Math.sqrt(Math.abs(xDC)))
-      ? Math.sqrt(xDE * xDE - xDL * xDL - xDC * xDC)
-      : 0;
-
-    const xSC = 1 + 0.045 * xC1;
-    const xSH = 1 + 0.015 * xC1;
-
-    xDL /= WEIGHT_L;
-    xDC /= WEIGHT_C * xSC;
-    xDH /= WEIGHT_H * xSH;
-
-    return Math.sqrt(xDL * xDL + xDC * xDC + xDH * xDH);
   }
 
   voted(): boolean {
