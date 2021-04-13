6c4cbabbe3cd4df6b1f452a42f06f0c04866c9f3
Tim <xmrtimex@gmail.com>
2021-04-13 20:18:08 +0200

    :art: Remove unused import

--- src/app/core/http/jamsession.http.service.ts
+++ src/app/core/http/jamsession.http.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {environment} from '../../../environments/environment';
 
 import {
   CreateJamSessionResponseBody,
