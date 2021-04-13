9e1f4b62230ecd273ba77dda783f6cb8775f1e52
Tim <xmrtimex@gmail.com>
2021-04-13 20:26:45 +0200

    :pencil2: Fix spelling mistake

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  JAM_API_PORT: '', // Should be handled a proxy backend
+  JAM_API_PORT: '', // Should be handled by a proxy
   JAM_WS_PROTOCOL: 'wss://'
 };
